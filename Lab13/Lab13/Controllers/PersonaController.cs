4c6f14c8d367cc42e3021c7c122474398985b512
AlejandroM <alejandro_mag0412@hotmail.com>
2017-11-07 10:30:45 -0500

    FInalizacion del laboratorio
    Finalizacion del laboratorio antes de las Actividades Adicionales

---
+++ Lab13/Lab13/Controllers/PersonaController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Lab13.Models;
+
+namespace Lab13.Controllers
+{
+    public class PersonaController : Controller
+    {
+        // GET: Persona
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Listar()
+        {
+            List<Persona> personas = new List<Persona>();
+            personas.Add(new Persona
+            {
+                PersonaID = 1,
+                Nombre = "Juan",
+                Apellido = "Perez",
+                Direccion = "AV. Evergreen 322",
+                FechaNac = Convert.ToDateTime("1997-11-07"),
+                Email = "juan@gmail.com"
+            });
+            personas.Add(new Persona
+            {
+                PersonaID = 2,
+                Nombre = "Maria",
+                Apellido = "Salas",
+                Direccion = "AV. Progreso 322",
+                FechaNac = Convert.ToDateTime("1995-11-07"),
+                Email = "salasn@gmail.com"
+            });
+            personas.Add(new Persona
+            {
+                PersonaID = 3,
+                Nombre = "Carlos",
+                Apellido = "Martinez",
+                Direccion = "AV. Manzanos 322",
+                FechaNac = Convert.ToDateTime("1992-11-07"),
+                Email = "cmartinez@gmail.com"
+            });
+            return View(personas);
+        }
+
+        public ActionResult Mostrar(int id)
+        {
+            List<Persona> personas = new List<Persona>();
+            personas.Add(new Persona
+            {
+                PersonaID = 1,
+                Nombre = "Juan",
+                Apellido = "Perez",
+                Direccion = "AV. Evergreen 322",
+                FechaNac = Convert.ToDateTime("1997-11-07"),
+                Email = "juan@gmail.com"
+            });
+            personas.Add(new Persona
+            {
+                PersonaID = 2,
+                Nombre = "Maria",
+                Apellido = "Salas",
+                Direccion = "AV. Progreso 322",
+                FechaNac = Convert.ToDateTime("1995-11-07"),
+                Email = "salasn@gmail.com"
+            });
+            personas.Add(new Persona
+            {
+                PersonaID = 3,
+                Nombre = "Carlos",
+                Apellido = "Martinez",
+                Direccion = "AV. Manzanos 322",
+                FechaNac = Convert.ToDateTime("1992-11-07"),
+                Email = "cmartinez@gmail.com"
+            });
+
+            Persona persona = (from p in personas
+                               where p.PersonaID == id
+                               select p).FirstOrDefault();
+            return View(persona);
+        }
+
+    }
+}
